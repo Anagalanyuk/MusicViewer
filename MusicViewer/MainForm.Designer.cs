39d63435b81ef1b4f773f1fea4f9eb2329c258e1
Anatoliy Anatoliy <anagalanyuk@gmail.com>
2018-08-03 00:45:50 +0300

    insert minimum and maximum date

---
+++ MusicViewer/MainForm.Designer.cs
@@ -0,0 +1,271 @@
+﻿namespace MusicViewer
+{
+	partial class MusicViewer
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.loadButton = new System.Windows.Forms.Button();
+			this.ListComposition = new System.Windows.Forms.ListBox();
+			this.MusicBox = new System.Windows.Forms.ComboBox();
+			this.label1 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.minimumDate = new System.Windows.Forms.DateTimePicker();
+			this.maximumDate = new System.Windows.Forms.DateTimePicker();
+			this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
+			this.album = new System.Windows.Forms.Label();
+			this.released = new System.Windows.Forms.Label();
+			this.length = new System.Windows.Forms.Label();
+			this.genres = new System.Windows.Forms.Label();
+			this.albumComposition = new System.Windows.Forms.Label();
+			this.releasedComposition = new System.Windows.Forms.Label();
+			this.lengthComposition = new System.Windows.Forms.Label();
+			this.genresComposition1 = new System.Windows.Forms.Label();
+			this.genresComposition2 = new System.Windows.Forms.Label();
+			this.genresComposition3 = new System.Windows.Forms.Label();
+			this.SuspendLayout();
+			// 
+			// loadButton
+			// 
+			this.loadButton.Location = new System.Drawing.Point(12, 12);
+			this.loadButton.Name = "loadButton";
+			this.loadButton.Size = new System.Drawing.Size(75, 23);
+			this.loadButton.TabIndex = 0;
+			this.loadButton.Text = "Load";
+			this.loadButton.UseVisualStyleBackColor = true;
+			this.loadButton.Click += new System.EventHandler(this.LoadButton_Click);
+			// 
+			// ListComposition
+			// 
+			this.ListComposition.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.ListComposition.FormattingEnabled = true;
+			this.ListComposition.ItemHeight = 16;
+			this.ListComposition.Location = new System.Drawing.Point(12, 93);
+			this.ListComposition.Name = "ListComposition";
+			this.ListComposition.Size = new System.Drawing.Size(255, 308);
+			this.ListComposition.TabIndex = 1;
+			this.ListComposition.SelectedValueChanged += new System.EventHandler(this.ListComposition_SelectedValueChanged);
+			// 
+			// MusicBox
+			// 
+			this.MusicBox.FormattingEnabled = true;
+			this.MusicBox.Location = new System.Drawing.Point(93, 14);
+			this.MusicBox.Name = "MusicBox";
+			this.MusicBox.Size = new System.Drawing.Size(424, 21);
+			this.MusicBox.TabIndex = 2;
+			this.MusicBox.SelectedIndexChanged += new System.EventHandler(this.MusicBox_SelectedIndexChanged);
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.label1.Location = new System.Drawing.Point(9, 49);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(42, 16);
+			this.label1.TabIndex = 3;
+			this.label1.Text = "From:";
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.label2.Location = new System.Drawing.Point(273, 49);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(28, 16);
+			this.label2.TabIndex = 5;
+			this.label2.Text = "To:";
+			// 
+			// minimumDate
+			// 
+			this.minimumDate.Location = new System.Drawing.Point(57, 49);
+			this.minimumDate.Name = "minimumDate";
+			this.minimumDate.Size = new System.Drawing.Size(210, 20);
+			this.minimumDate.TabIndex = 6;
+			this.minimumDate.Value = new System.DateTime(2018, 8, 2, 1, 2, 17, 0);
+			this.minimumDate.Visible = false;
+			this.minimumDate.ValueChanged += new System.EventHandler(this.MinimumDate_ValueChanged);
+			// 
+			// maximumDate
+			// 
+			this.maximumDate.Location = new System.Drawing.Point(307, 49);
+			this.maximumDate.MaxDate = new System.DateTime(2018, 8, 2, 0, 0, 0, 0);
+			this.maximumDate.MinDate = new System.DateTime(2018, 6, 25, 0, 0, 0, 0);
+			this.maximumDate.Name = "maximumDate";
+			this.maximumDate.Size = new System.Drawing.Size(210, 20);
+			this.maximumDate.TabIndex = 7;
+			this.maximumDate.Value = new System.DateTime(2018, 8, 2, 0, 0, 0, 0);
+			this.maximumDate.Visible = false;
+			this.maximumDate.ValueChanged += new System.EventHandler(this.MaximumDate_ValueChanged);
+			// 
+			// album
+			// 
+			this.album.AutoSize = true;
+			this.album.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.album.Location = new System.Drawing.Point(290, 93);
+			this.album.Name = "album";
+			this.album.Size = new System.Drawing.Size(58, 20);
+			this.album.TabIndex = 8;
+			this.album.Text = "Album:";
+			// 
+			// released
+			// 
+			this.released.AutoSize = true;
+			this.released.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.released.Location = new System.Drawing.Point(267, 135);
+			this.released.Name = "released";
+			this.released.Size = new System.Drawing.Size(81, 20);
+			this.released.TabIndex = 9;
+			this.released.Text = "Released:";
+			// 
+			// length
+			// 
+			this.length.AutoSize = true;
+			this.length.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.length.Location = new System.Drawing.Point(285, 183);
+			this.length.Name = "length";
+			this.length.Size = new System.Drawing.Size(63, 20);
+			this.length.TabIndex = 10;
+			this.length.Text = "Length:";
+			// 
+			// genres
+			// 
+			this.genres.AutoSize = true;
+			this.genres.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.genres.Location = new System.Drawing.Point(282, 232);
+			this.genres.Name = "genres";
+			this.genres.Size = new System.Drawing.Size(66, 20);
+			this.genres.TabIndex = 11;
+			this.genres.Text = "Genres:";
+			// 
+			// albumComposition
+			// 
+			this.albumComposition.AutoSize = true;
+			this.albumComposition.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.albumComposition.Location = new System.Drawing.Point(355, 93);
+			this.albumComposition.Name = "albumComposition";
+			this.albumComposition.Size = new System.Drawing.Size(0, 20);
+			this.albumComposition.TabIndex = 12;
+			// 
+			// releasedComposition
+			// 
+			this.releasedComposition.AutoSize = true;
+			this.releasedComposition.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.releasedComposition.Location = new System.Drawing.Point(354, 135);
+			this.releasedComposition.Name = "releasedComposition";
+			this.releasedComposition.Size = new System.Drawing.Size(0, 20);
+			this.releasedComposition.TabIndex = 13;
+			// 
+			// lengthComposition
+			// 
+			this.lengthComposition.AutoSize = true;
+			this.lengthComposition.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.lengthComposition.Location = new System.Drawing.Point(355, 183);
+			this.lengthComposition.Name = "lengthComposition";
+			this.lengthComposition.Size = new System.Drawing.Size(0, 20);
+			this.lengthComposition.TabIndex = 14;
+			// 
+			// genresComposition1
+			// 
+			this.genresComposition1.AutoSize = true;
+			this.genresComposition1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.genresComposition1.Location = new System.Drawing.Point(355, 232);
+			this.genresComposition1.Name = "genresComposition1";
+			this.genresComposition1.Size = new System.Drawing.Size(0, 20);
+			this.genresComposition1.TabIndex = 15;
+			// 
+			// genresComposition2
+			// 
+			this.genresComposition2.AutoSize = true;
+			this.genresComposition2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.genresComposition2.Location = new System.Drawing.Point(355, 263);
+			this.genresComposition2.Name = "genresComposition2";
+			this.genresComposition2.Size = new System.Drawing.Size(0, 20);
+			this.genresComposition2.TabIndex = 16;
+			// 
+			// genresComposition3
+			// 
+			this.genresComposition3.AutoSize = true;
+			this.genresComposition3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.genresComposition3.Location = new System.Drawing.Point(355, 293);
+			this.genresComposition3.Name = "genresComposition3";
+			this.genresComposition3.Size = new System.Drawing.Size(0, 20);
+			this.genresComposition3.TabIndex = 17;
+			// 
+			// MusicViewer
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+			this.ClientSize = new System.Drawing.Size(523, 432);
+			this.Controls.Add(this.genresComposition3);
+			this.Controls.Add(this.genresComposition2);
+			this.Controls.Add(this.genresComposition1);
+			this.Controls.Add(this.lengthComposition);
+			this.Controls.Add(this.releasedComposition);
+			this.Controls.Add(this.albumComposition);
+			this.Controls.Add(this.genres);
+			this.Controls.Add(this.length);
+			this.Controls.Add(this.released);
+			this.Controls.Add(this.album);
+			this.Controls.Add(this.maximumDate);
+			this.Controls.Add(this.minimumDate);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.MusicBox);
+			this.Controls.Add(this.ListComposition);
+			this.Controls.Add(this.loadButton);
+			this.MaximizeBox = false;
+			this.Name = "MusicViewer";
+			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+			this.Text = "Music Viewer";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Button loadButton;
+		private System.Windows.Forms.ListBox ListComposition;
+		private System.Windows.Forms.ComboBox MusicBox;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.DateTimePicker minimumDate;
+		private System.Windows.Forms.DateTimePicker maximumDate;
+		private System.Windows.Forms.OpenFileDialog openFileDialog;
+		private System.Windows.Forms.Label album;
+		private System.Windows.Forms.Label released;
+		private System.Windows.Forms.Label length;
+		private System.Windows.Forms.Label genres;
+		private System.Windows.Forms.Label albumComposition;
+		private System.Windows.Forms.Label releasedComposition;
+		private System.Windows.Forms.Label lengthComposition;
+		private System.Windows.Forms.Label genresComposition1;
+		private System.Windows.Forms.Label genresComposition2;
+		private System.Windows.Forms.Label genresComposition3;
+	}
+}
+
---
+++ MusicViewer/MainForm.cs
@@ -0,0 +1,268 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace MusicViewer
+{
+	internal partial class MusicViewer : Form
+	{
+		private XmlNode compositions;
+		private XmlNode list;
+		private XmlDocument musicList = new XmlDocument();
+		private string number;
+		private string path;
+
+		public MusicViewer()
+		{
+			InitializeComponent();
+		}
+
+		public void OpenXml(string path)
+		{
+			List<string> artists = new List<string>();
+			musicList.Load(path);
+			XmlNode name = musicList.DocumentElement;
+
+			foreach (XmlNode title in name.ChildNodes)
+			{
+				foreach (XmlNode track in title.ChildNodes)
+				{
+					if (track.Attributes.Count > 0)
+					{
+						XmlNode artist = track.Attributes.GetNamedItem("name");
+						if (artist != null)
+						{
+							if (track.Name == "artist")
+							{
+								artists.Add(artist.Value);
+							}
+						}
+					}
+				}
+			}
+
+			artists.Sort();
+			foreach (string art in artists)
+			{
+				MusicBox.Items.Add(art);
+			}
+		}
+
+		private void LoadButton_Click(object sender, EventArgs e)
+		{
+			if (openFileDialog.ShowDialog() == DialogResult.OK)
+			{
+				path = openFileDialog.FileName;
+				OpenXml(path);
+			}
+		}
+
+		private void MusicBox_SelectedIndexChanged(object sender, EventArgs e)
+		{
+			albumComposition.Text = "";
+			releasedComposition.Text = "";
+			lengthComposition.Text = "";
+			genresComposition1.Text = "";
+			genresComposition2.Text = "";
+			genresComposition3.Text = "";
+			MinimumMaximumDate();
+		}
+
+		private void MinimumMaximumDate()
+		{
+			List<string> listcompositions = new List<string>();
+			if (ListComposition.Items.Count != 0)
+			{
+				ListComposition.Items.Clear();
+			}
+
+			compositions = musicList.DocumentElement;
+			list = compositions.LastChild;
+
+			foreach (XmlNode artist in compositions.ChildNodes)
+			{
+				if (artist.Name == "artists")
+				{
+					foreach (XmlNode idArtist in artist.ChildNodes)
+					{
+						if (idArtist.Attributes.GetNamedItem("name").Value == MusicBox.Items[MusicBox.SelectedIndex].ToString())
+						{
+							number = idArtist.Attributes.GetNamedItem("id").Value;
+							break;
+						}
+					}
+				}
+			}
+
+			bool minimum = true;
+			foreach (XmlNode track in list.ChildNodes)
+			{
+				if (list.ChildNodes.Count != 0)
+				{
+					if (track.Attributes.GetNamedItem("artist-id").Value == number)
+					{
+						foreach (XmlNode comp in compositions.ChildNodes)
+						{
+							if (comp.Name == "artists")
+							{
+								foreach (XmlNode artistId in comp.ChildNodes)
+								{
+									if (artistId.Attributes.GetNamedItem("id").Value == number)
+									{
+										//listcompositions.Add(track.Attributes.GetNamedItem("name").Value);
+										string[] dateComp = track.Attributes.GetNamedItem("released").Value.Split('.');
+										int dateDay = Convert.ToInt32(dateComp[0]);
+										int dateMonth = Convert.ToInt32(dateComp[1]);
+										int dateYear = Convert.ToInt32(dateComp[2]);
+										if (minimum)
+										{
+											minimumDate.MaxDate = DateTime.Now;
+											maximumDate.MaxDate = DateTime.Now;
+											minimumDate.MinDate = new DateTime(dateYear, dateMonth, dateDay);
+											maximumDate.MinDate = new DateTime(dateYear, dateMonth, dateDay);
+											minimumDate.MaxDate = new DateTime(dateYear, dateMonth, dateDay);// DateTime.Now;
+											maximumDate.MaxDate = new DateTime(dateYear, dateMonth, dateDay);// DateTime.Now;
+											minimum = false;
+										}
+										string[] array = minimumDate.MinDate.ToShortDateString().Split('.');
+										int day = Convert.ToInt32(array[0]);
+										int month = Convert.ToInt32(array[1]);
+										int year = Convert.ToInt32(array[2]);
+
+										if (dateYear < year)
+										{
+											minimumDate.MinDate = new DateTime(dateYear, dateMonth, dateDay);
+											maximumDate.MinDate = new DateTime(dateYear, dateMonth, dateDay);
+										}
+										else if (dateYear == year)
+										{
+											if (dateMonth < month)
+											{
+												minimumDate.MinDate = new DateTime(dateYear, dateMonth, dateDay);
+												maximumDate.MinDate = new DateTime(dateYear, dateMonth, dateDay);
+											}
+											else if (dateMonth == month)
+											{
+												if (dateDay < day)
+												{
+													minimumDate.MinDate = new DateTime(dateYear, dateMonth, dateDay);
+													maximumDate.MinDate = new DateTime(dateYear, dateMonth, dateDay);
+												}
+											}
+										}
+										if (dateYear > year)
+										{
+											minimumDate.MaxDate = new DateTime(dateYear, dateMonth, dateDay);
+											maximumDate.MaxDate = new DateTime(dateYear, dateMonth, dateDay);
+										}
+										else if (dateYear == year)
+										{
+											if (dateMonth > month)
+											{
+												minimumDate.MaxDate = new DateTime(dateYear, dateMonth, dateDay);
+												maximumDate.MaxDate = new DateTime(dateYear, dateMonth, dateDay);
+											}
+											else if (dateMonth == month)
+											{
+												if (dateDay > day)
+												{
+													minimumDate.MaxDate = new DateTime(dateYear, dateMonth, dateDay);
+													maximumDate.MaxDate = new DateTime(dateYear, dateMonth, dateDay);
+												}
+												//else
+												//{
+												//	minimumDate.MaxDate = minimumDate.MinDate;
+												//	maximumDate.MaxDate = minimumDate.MinDate;
+												//}
+											}
+										}
+									}
+								}
+							}
+						}
+					}
+				}
+			}
+
+			listcompositions.Sort();
+			foreach (string comp in listcompositions)
+			{
+				ListComposition.Items.Add(comp);
+			}
+			minimumDate.Value = minimumDate.MinDate;
+			maximumDate.Value = maximumDate.MaxDate;
+			minimumDate.Visible = true;
+			maximumDate.Visible = true;
+		}
+
+		private void ListComposition_SelectedValueChanged(object sender, EventArgs e)
+		{
+			genresComposition1.Text = "";
+			genresComposition2.Text = "";
+			genresComposition3.Text = "";
+
+			foreach (XmlNode track in list.ChildNodes)
+			{
+				if (track.Attributes.GetNamedItem("name").Value == ListComposition.SelectedItem)
+				{
+					foreach (XmlNode albums in compositions.ChildNodes)
+					{
+						if (albums.Name == "albums")
+						{
+							foreach (XmlNode album in albums.ChildNodes)
+							{
+								if (album.Attributes.GetNamedItem("id").Value == track.Attributes.GetNamedItem("album-id").Value)
+								{
+									albumComposition.Text = album.Attributes.GetNamedItem("name").Value;
+								}
+							}
+						}
+					}
+
+					lengthComposition.Text = track.Attributes.GetNamedItem("length").Value;
+					releasedComposition.Text = track.Attributes.GetNamedItem("released").Value;
+					XmlNode genre = track.FirstChild;
+
+					foreach (XmlNode idGenre in genre)
+					{
+						foreach (XmlNode genres in compositions.ChildNodes)
+						{
+							if (genres.Name == "genres")
+							{
+								foreach (XmlNode id in genres.ChildNodes)
+								{
+									if (id.Attributes.GetNamedItem("id").Value == idGenre.Attributes.GetNamedItem("genre-id").Value)
+									{
+										if (genresComposition1.Text.Length == 0)
+										{
+											genresComposition1.Text = id.Attributes.GetNamedItem("name").Value;
+										}
+										else if (genresComposition2.Text.Length == 0)
+										{
+											genresComposition2.Text = id.Attributes.GetNamedItem("name").Value;
+										}
+										else if (genresComposition3.Text.Length == 0)
+										{
+											genresComposition3.Text = id.Attributes.GetNamedItem("name").Value;
+										}
+									}
+								}
+							}
+						}
+					}
+				}
+			}
+		}
+
+		private void MaximumDate_ValueChanged(object sender, EventArgs e)
+		{
+			//ListCompositions();
+		}
+
+		private void MinimumDate_ValueChanged(object sender, EventArgs e)
+		{
+			//ListCompositions();
+		}
+	}
+}
